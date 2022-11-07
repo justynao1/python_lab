b284ca110766a7777acb7b3aaa1f03775374201d
justynao1 <justyna_opyd@icloud.com>
2022-11-07 19:41:41 +0100

    6.11

--- ComplexCalculator.py
+++ ComplexCalculator.py
@@ -1,15 +1,40 @@
-def Complex():
-    def __init__(self, realpart, imagpart):
-        self.re = realpart
-        self.im = imagpart
+def Calculator():
 
-    def __add__(self, other):
-        return Complex(self.re + other.re, self.im + other.im)
+    def Complex():
+        def __init__(self, realpart, imagpart):
+            self.re = realpart
+            self.im = imagpart
 
-    def __sub__(self, other):
-        return Complex(self.re - other.re, self.im - other.im)
+        def __add__(self, other):
+            return Complex(self.re + other.re, self.im + other.im)
+
+        def __sub__(self, other):
+            return Complex(self.re - other.re, self.im - other.im)
+
+        def __mult__(self, other):
+            return Complex()
+
+    comp1 = input("Input the first number: ")
+    comp1 = complex(comp1)
+    op = input("Enter an operation (+, -, * or /): ")
+    comp2 = input("Input the second number: ")
+    comp2 = complex(comp2)
+
+    def operations():
+        if op == "+":
+            result = comp1 + comp2
+        elif op == "-":
+            result = comp1 - comp2
+        elif op == "*":
+            result = comp1 * comp2
+        elif op == "/":
+            result = comp1 / comp2
+        try:
+            print(result)
+        except NameError:
+            print("Unknown operation")
+
+    operations()
 
 
-comp1 = complex(2, 3)
-comp2 = complex(5, -2)
-
+Calculator()
