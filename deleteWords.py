1adf142cda9749a15a8eb3c35b850601ff265e11
justynao1 <justyna_opyd@icloud.com>
2022-11-08 15:08:55 +0100

    6.11

--- deleteWords.py
+++ deleteWords.py
@@ -1,12 +1,4 @@
 # usuwanie słów
-"""
-def deleteWords():
-    # text = r'/Users/macbook/downloads/text_files/italy/Amber.txt'
-    with open('/Users/macbook/downloads/text_files/italy/Amidah.txt', 'r') as file:
-        new_text = '/Users/macbook/downloads/text_files/italy/Amidah.txt'.replace('is', "")
-        file.write(new_text)
-        print(new_text)
-"""
 
 def deleteWords():
     file1 = "/Users/macbook/downloads/text_files/france/London.txt"
